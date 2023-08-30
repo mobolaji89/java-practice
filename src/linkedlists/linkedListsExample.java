3568ab86950f976b19119a6618a89b7f74ce4894
Mobolaji David <davidmo@colorado.edu>
2023-08-29 20:43:47 -0700

    basic linked lists example

---
+++ src/linkedlists/linkedListsExample.java
@@ -0,0 +1,27 @@
+package linkedlists;
+
+public class linkedListsExample {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode (int val) {
+            this.val = val;
+        }
+    }
+    
+    public static void main(String[] args) {
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(4);
+        one.next = two;
+        two.next = three;
+        three.next = four;
+        ListNode head = one;
+        
+        System.out.println(head.val);
+        System.out.println(head.next.val);
+        System.out.println(head.next.next.val);
+        System.out.println(head.next.next.next.val);
+    }
+}
