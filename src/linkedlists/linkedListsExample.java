0e775ca8759a12f4e556ef5ec64c829bfa5e9376
Mobolaji David <davidmo@colorado.edu>
2023-08-31 16:27:51 -0700

    getSum and getSumRecursive functions for linked list

--- src/linkedlists/linkedListsExample.java
+++ src/linkedlists/linkedListsExample.java
@@ -9,6 +9,26 @@ public class linkedListsExample {
         }
     }
     
+    // Function to get the sum of all values from an integer linked list
+    public static int getSum(ListNode head) {
+        int ans = 0;
+        while (head != null) {
+            ans += head.val;
+            head = head.next;
+        }
+
+        return ans;
+    }
+    
+    // Function to get the sum of all values from an integer linked list recursively
+    public static int getSumRecursive(ListNode head) {
+        if (head == null) {
+            return 0;
+        }
+
+        return head.val + getSumRecursive(head.next);
+    }
+    
     public static void main(String[] args) {
         ListNode one = new ListNode(1);
         ListNode two = new ListNode(2);
@@ -23,5 +43,11 @@ public class linkedListsExample {
         System.out.println(head.next.val);
         System.out.println(head.next.next.val);
         System.out.println(head.next.next.next.val);
+        
+        // Call getSum function
+        System.out.println(getSum(head));
+        
+        // Call getSumRecursive function
+        System.out.println(getSumRecursive(head));
     }
 }
